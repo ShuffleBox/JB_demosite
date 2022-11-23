4b55d2c328dfe7f328ad5257ac9108f2a10cfc5f
Garret Rumohr <garretr@gmail.com>
2022-11-23 13:24:48 -0600

    pull out haystack settings

--- jbweb/jbweb/settings.py
+++ jbweb/jbweb/settings.py
@@ -204,13 +204,5 @@ MESSAGE_TAGS = {message_constants.DEBUG: 'debug',
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #Haystack Solr search stuff
-HAYSTACK_CONNECTIONS = {
-'default': {
-'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
-'URL': 'http://webfile01.rumohr.me:8983/solr/somejunk', 
-'ADMIN_URL': 'http://webfile01.rumohr.me:8983/solr/admin/cores',
-'TIMEOUT': 60 * 5,
-# ...or for multicore...
-# 'URL': 'http://127.0.0.1:8983/solr/mysite',
-},
-}
+HAYSTACK_CONNECTIONS = get_conf_variable('HAYSTACK_CONNECTIONS')
+
